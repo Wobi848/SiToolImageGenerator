adeffb7dae82cd8906df18934e97a9c7a94a0daa
wobi848 <torappo@gmail.com>
2024-10-26 19:32:40 +0200

    v0.7.16 metaa

--- src/generator/script/main.js
+++ src/generator/script/main.js
@@ -1,6 +1,6 @@
 "use strict";
 // Version
-const version = "0.7.15";
+const version = "0.7.16";
 const versionElement = document.getElementById("version");
 if (versionElement)
     versionElement.innerText = `v${version}`;
